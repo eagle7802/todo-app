cbbb68c4730c09a369ec08bc1e16ed9090eb5be2
eagle7802 <kanat.kadirov7@gmail.com>
2024-09-10 11:52:51 +0500

    base structure project

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/eagle7802/todo-app"
+	"log"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("Error occured while running http server: %s", err.Error())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eagle7802/todo-app
+
+go 1.22.6
