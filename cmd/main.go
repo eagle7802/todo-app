f30a2c9dba9ce11b32fa42df070da41cb73efacc
eagle7802 <kanat.kadirov7@gmail.com>
2024-09-11 14:14:04 +0500

    base structure project

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/eagle7802/todo-app"
+	handler "github.com/eagle7802/todo-app/pkg/handlers"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 
---
+++ pkg/handlers/auth.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) signUp(c *gin.Context) {
+	return
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+	return
+}
