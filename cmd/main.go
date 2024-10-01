541706812628838cff43f90ef3a4587878467040
eagle7802 <kanat.kadirov7@gmail.com>
2024-10-01 13:58:38 +0500

    connecting to a db an application, env variable

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 	handler "github.com/eagle7802/todo-app/pkg/handlers"
 	"github.com/eagle7802/todo-app/pkg/repository"
 	"github.com/eagle7802/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("Error initializing config: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("Error initializing db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 
 	handlers := handler.NewHandler(services)
